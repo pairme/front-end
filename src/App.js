5849cb47df24601c89325f57970dcc74853ae838
Brad Mortensen <b.mort86@gmail.com>
2019-07-01 10:42:28 -0600

    inits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ChatContainer from './components/ChatContainer'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  margin: 0 auto;
+  width: 1000px;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp className="App">
+      <ChatContainer />
+    </StyledApp>
   );
 }
 
---
+++ src/components/ChatContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Message from './Message'
+import ChatInput from './ChatInput.js'
+
+const StyledChat = styled.div`
+  border: 1px solid black;
+  width: 50%;
+  height: 500px;
+  margin-top: 100px;
+`;
+
+const ChatContainer = () => {
+  const messages = ['hi', 'hello', 'lets pair program', 'sounds good', 'https://us04web.zoom.us/j/553930217']
+  return (
+    <StyledChat>
+      {messages.map(msg=><Message msg={msg}/>)}
+      <ChatInput/>
+    </StyledChat>
+  )
+}
+
+export default ChatContainer
