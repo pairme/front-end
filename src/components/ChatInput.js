6462f4a254ea635fb88a0473f357e4fa2723c052
CarloC24 <carlo.clamucha@gmail.com>
2019-07-02 20:32:33 -0500

    added stop typing

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const StyledInput = styled.form`
   width: 100%;
@@ -7,7 +7,7 @@ const StyledInput = styled.form`
   width: 60%;
   height: 40px;
   box-sizing: border-box;
-  display: flex; 
+  display: flex;
   flex-flow: row;
   input {
     width: 70%;
@@ -18,26 +18,41 @@ const StyledInput = styled.form`
     border: 1px solid black;
     border-left: 0px;
   }
-`
+`;
+let timeout = null;
 
 const ChatInput = ({ submitMessage, makePair, buttonDisabled, totalUsers }) => {
-  const [message, setMessage] = useState('')
-  const messageHandler = (e) => {
-    submitMessage(e, message)
-    setMessage('')
-  }
+  const [message, setMessage] = useState("");
+  const messageHandler = e => {
+    submitMessage(e, message);
+    setMessage("");
+  };
   return (
     <StyledInput onSubmit={messageHandler}>
       <input
         onChange={e => setMessage(e.target.value)}
         placeholder="Type your message..."
-        value={message} />
-      <button type="submit" >Send</button>
-      {buttonDisabled || totalUsers<2 ? <button type="button" disabled>Pair</button> : <button type="button" onClick={makePair} >Pair</button>}
-
-
+        value={message}
+        onKeyDown={e => console.log("keydown")}
+        onKeyUp={e => {
+          clearTimeout(timeout);
+          timeout = setTimeout(function() {
+            console.log("he stopped typing");
+          }, 500);
+        }}
+      />
+      <button type="submit">Send</button>
+      {buttonDisabled || totalUsers < 2 ? (
+        <button type="button" disabled>
+          Pair
+        </button>
+      ) : (
+        <button type="button" onClick={makePair}>
+          Pair
+        </button>
+      )}
     </StyledInput>
-  )
-}
+  );
+};
 
-export default ChatInput
+export default ChatInput;
