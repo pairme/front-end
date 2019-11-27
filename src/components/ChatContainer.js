d46b20ed1014aadf2d261d029b93a4d3fda4535e
Brad Mortensen <b.mort86@gmail.com>
2019-11-27 14:53:10 -0700

    formatting chat container

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -5,6 +5,41 @@ import logo from '../pairme-logo.png'
 import Message from "./Message";
 import ChatInput from "./ChatInput.js";
 
+
+const ChatContainer = ({ messages, submitMessage, userName, makePair, socket, buttonDisabled, totalUsers }) => {
+  const [typers, setTypers] = useState([])
+
+  useEffect(() => {
+    socket.emit('add connection', userName)
+    console.log('add connection effect rerendered!!!')
+  }, [socket, userName])
+  useEffect(() => {
+    socket.on('typing users', users => setTypers([...users]))
+    console.log('typers', typers)
+    console.log('typing effect rerendered!!!')
+  }, [socket])
+  return (
+    <>
+      <StyledChat>
+        <div className="header">
+          <img src={logo} alt="pairme logo" />
+        </div>
+        <p>{totalUsers} Users Connected</p>
+        {messages.map(msg => (
+          <Message msg={msg} userName={userName} key={msg.id} />
+        ))}
+
+      </StyledChat>
+      <StyledTypers className="typers">
+        Typing:{typers.map(typer => <p key={typer}>{typer}{' '}</p>)}
+      </StyledTypers>
+      <ChatInput buttonDisabled={buttonDisabled} userName={userName} socket={socket} totalUsers={totalUsers} submitMessage={submitMessage} makePair={makePair} />
+    </>
+  );
+};
+
+export default ChatContainer;
+
 const StyledChat = styled.div`
   width: 100%;
   height: 86%;
@@ -39,37 +74,3 @@ const StyledTypers = styled.div`
   font-size: .8rem;
   box-sizing: border-box;
 `;
-const spaceVar = ' '
-const ChatContainer = ({ messages, submitMessage, userName, makePair, socket, buttonDisabled, totalUsers }) => {
-  const [typers, setTypers] = useState([])
-
-  useEffect(() => {
-    socket.emit('add connection', userName)
-    console.log('add connection effect rerendered!!!')
-  }, [socket, userName])
-  useEffect(() => {
-    socket.on('typing users', users => setTypers([...users]))
-    console.log('typers', typers)
-    console.log('typing effect rerendered!!!')
-  }, [socket])
-  return (
-    <>
-      <StyledChat>
-        <div className="header">
-          <img src={logo} alt="pairme logo" />
-        </div>
-        <p>{totalUsers} Users Connected</p>
-        {messages.map(msg => (
-          <Message msg={msg} userName={userName} key={msg.id} />
-        ))}
-
-      </StyledChat>
-      <StyledTypers className="typers">
-        Typing:{typers.map(typer => <p key={typer}>{typer}{' '}</p>)}
-      </StyledTypers>
-      <ChatInput buttonDisabled={buttonDisabled} userName={userName} socket={socket} totalUsers={totalUsers} submitMessage={submitMessage} makePair={makePair} />
-    </>
-  );
-};
-
-export default ChatContainer;
